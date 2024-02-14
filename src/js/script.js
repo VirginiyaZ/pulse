c1c58f6d361210bb072c5987a77fd92016b38809
Virgy <email@gmail.com>
2024-02-14 15:48:42 +0500

    creating a modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -61,6 +61,22 @@ $(document).ready(function() {
   //   });
   // });
 
+  //modal
+
+  $('[data-modal=consultation]').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #popup, #order').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
 });
 
 
